617572711f4ee58f2069f4d6e0ffbeabc52c3ed4
Mohammad Taufik Andrian <57611731+taufikandrian18@users.noreply.github.com>
2024-12-04 08:36:07 +0000

    Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,71 +9,86 @@ import { gradientBgPurplePink } from '../colors'
 import { appTitle } from '../config'
 import { useAppDispatch } from '../stores/hooks'
 import { setDarkMode } from '../stores/darkModeSlice'
+import { getPageTitle } from '../config'
+import SectionFullScreen from '../components/Section/FullScreen'
+import { Field, Form, Formik } from 'formik'
+import FormField from '../components/Form/Field'
+import FormCheckRadio from '../components/Form/CheckRadio'
+import Divider from '../components/Divider'
+import Buttons from '../components/Buttons'
+import Button from '../components/Button'
 
-const StyleSelectPage = () => {
+type LoginForm = {
+  login: string
+  password: string
+  remember: boolean
+}
+
+const LoginPage = () => {
   const dispatch = useAppDispatch()
+
+  const initialValues: LoginForm = {
+    login: 'john.doe',
+    password: 'bG1sL9eQ1uD2sK3b',
+    remember: true,
+  }
 
   dispatch(setDarkMode(false))
 
-  const styles = ['white', 'basic']
+  const styles = 'basic'
 
   const router = useRouter()
 
-  const handleStylePick = (e: React.MouseEvent, style: string) => {
-    e.preventDefault()
+  const handleSubmit = (formValues: LoginForm) => {
 
     document.documentElement.classList.forEach((token) => {
       if (token.indexOf('style') === 0) {
-        document.documentElement.classList.replace(token, `style-${style}`)
+        document.documentElement.classList.replace(token, `style-${styles}`)
       }
     })
 
     router.push('/dashboard')
+    console.log('Form values', formValues)
   }
 
   return (
     <>
       <Head>
-        <title>{appTitle}</title>
+        <title>{getPageTitle('Login')}</title>
       </Head>
-      <div className={`flex min-h-screen items-center justify-center ${gradientBgPurplePink}`}>
-        <SectionMain>
-          <h1 className="text-4xl md:text-5xl text-center text-white font-bold mt-12 mb-3 lg:mt-0">
-            Pick a style&hellip;
-          </h1>
-          <h2 className="text-xl md:text-xl text-center text-white mb-12">
-            Style switching with a single{' '}
-            <code className="px-1.5 py-0.5 rounded bg-white bg-opacity-20">action()</code>
-          </h2>
-          <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 px-6 max-w-6xl mx-auto">
-            {styles.map((style) => (
-              <CardBox
-                key={style}
-                className="cursor-pointer bg-gray-50"
-                isHoverable
-                onClick={(e) => handleStylePick(e, style)}
-              >
-                <div className="mb-3 md:mb-6">
-                  <Image
-                    src={`https://static.justboil.me/templates/one/small/${style}-v3.png`}
-                    width={1280}
-                    height={720}
-                    alt={style}
-                  />
-                </div>
-                <h1 className="text-xl md:text-2xl font-black capitalize">{style}</h1>
-                <h2 className="text-lg md:text-xl">& Dark mode</h2>
-              </CardBox>
-            ))}
-          </div>
-        </SectionMain>
-      </div>
+
+      <SectionFullScreen bg="purplePink">
+        <CardBox className="w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 shadow-2xl">
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Form>
+              <FormField label="Login" help="Please enter your login">
+                <Field name="login" />
+              </FormField>
+
+              <FormField label="Password" help="Please enter your password">
+                <Field name="password" type="password" />
+              </FormField>
+
+              <FormCheckRadio type="checkbox" label="Remember">
+                <Field type="checkbox" name="remember" />
+              </FormCheckRadio>
+
+              <Divider />
+
+              <Buttons>
+                <Button type="submit" label="Login" color="info" />
+                <Button href="/dashboard" label="Home" color="info" outline />
+              </Buttons>
+            </Form>
+          </Formik>
+        </CardBox>
+      </SectionFullScreen>
     </>
   )
 }
 
-StyleSelectPage.getLayout = function getLayout(page: ReactElement) {
+LoginPage.getLayout = function getLayout(page: ReactElement) {
   return <LayoutGuest>{page}</LayoutGuest>
 }
 
-export default StyleSelectPage
+export default LoginPage
